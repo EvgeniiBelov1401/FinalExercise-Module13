32089ac701a94be265b4cd8aba6a1133af580190
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 09:25:48 +0300

    Считал текст из файла и записал его в List

---
+++ FinalExercise/Exercise-13-06-01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_13_06_01
+{
+
+//    Задание 13.6.1
+//Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте уже знакомый вам StopWatch.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Считываем текст из файла
+            string path = @"D:\Programming\Skillfactory\C#_projects\Module_13\FinalExercise\FinalExercise\Exercise-13-06-01\Text1.txt";
+            string originalText=File.ReadAllText(path);
+            #endregion
+
+
+            string[] textWords = originalText.Split(' ');
+            var ListWords=new List<string>(textWords);
+           
+            
+        }
+    }
+}
