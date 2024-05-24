7adc42b66d05a8c32c687ff52f833a2eb3e6bd79
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 10:03:42 +0300

    Расчет времени вставки для List

--- FinalExercise/Exercise-13-06-01/Program.cs
+++ FinalExercise/Exercise-13-06-01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise_13_06_01
+﻿using System.Diagnostics;
+
+namespace Exercise_13_06_01
 {
 
 //    Задание 13.6.1
@@ -7,16 +9,27 @@
     {
         static void Main(string[] args)
         {
-            #region Считываем текст из файла
-            string path = @"D:\Programming\Skillfactory\C#_projects\Module_13\FinalExercise\FinalExercise\Exercise-13-06-01\Text1.txt";
-            string originalText=File.ReadAllText(path);
+            var timeListInsert=new Stopwatch();
+            #region Считываем текст(оригинал и для вставки) из файла и переводим в List
+            string pathOriginalText = @"D:\Programming\Skillfactory\C#_projects\Module_13\FinalExercise\FinalExercise\Exercise-13-06-01\Text1.txt";
+            string pathInsertText = @"D:\Programming\Skillfactory\C#_projects\Module_13\FinalExercise\FinalExercise\Exercise-13-06-01\О Гончарове.txt";
+            
+            string originalText=File.ReadAllText(pathOriginalText);
+            string insertText=File.ReadAllText(pathInsertText);
+
+            string[] originalTextWords = originalText.Split(" ");
+            
+            var ListOriginalWords = new List<string>(originalTextWords);
+            
             #endregion
 
 
-            string[] textWords = originalText.Split(' ');
-            var ListWords=new List<string>(textWords);
-           
-            
+            timeListInsert.Start();
+            ListOriginalWords.Insert(1,insertText);
+            timeListInsert.Stop();
+
+            Console.WriteLine($"Время вставки для List: {timeListInsert.Elapsed.TotalNanoseconds} нс");
+
         }
     }
 }
