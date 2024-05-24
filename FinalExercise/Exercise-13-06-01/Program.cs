fe69a7cab9302a9c8c4c31006aef8a798353502e
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-24 10:38:22 +0300

    вставка в LinkedList

--- FinalExercise/Exercise-13-06-01/Program.cs
+++ FinalExercise/Exercise-13-06-01/Program.cs
@@ -10,6 +10,8 @@ namespace Exercise_13_06_01
         static void Main(string[] args)
         {
             var timeListInsert=new Stopwatch();
+            var timeLinkedListInsert=new Stopwatch();
+
             #region Считываем текст(оригинал и для вставки) из файла и переводим в List
             string pathOriginalText = @"D:\Programming\Skillfactory\C#_projects\Module_13\FinalExercise\FinalExercise\Exercise-13-06-01\Text1.txt";
             string pathInsertText = @"D:\Programming\Skillfactory\C#_projects\Module_13\FinalExercise\FinalExercise\Exercise-13-06-01\О Гончарове.txt";
@@ -20,16 +22,39 @@ namespace Exercise_13_06_01
             string[] originalTextWords = originalText.Split(" ");
             
             var ListOriginalWords = new List<string>(originalTextWords);
-            
+            var LinkedListOriginalWords= new LinkedList<string>(originalTextWords);
             #endregion
 
-
+            #region Вставка текста в List
             timeListInsert.Start();
-            ListOriginalWords.Insert(1,insertText);
+            ListOriginalWords.Insert(100,insertText);
             timeListInsert.Stop();
+            #endregion
 
+            #region Вставка текста в LinkedList
+            timeLinkedListInsert.Start();
+            LinkedListNode<string> node = LinkedListOriginalWords.Find("и");
+            LinkedListOriginalWords.AddAfter(node,insertText);
+            timeLinkedListInsert.Stop();
+            #endregion
+
+            #region Вывод результата
             Console.WriteLine($"Время вставки для List: {timeListInsert.Elapsed.TotalNanoseconds} нс");
-
+            Console.WriteLine($"Время вставки для LinkedList: {timeLinkedListInsert.Elapsed.TotalNanoseconds} нс\n");
+            
+            if (timeLinkedListInsert.Elapsed.TotalNanoseconds< timeListInsert.Elapsed.TotalNanoseconds)
+            {
+                Console.WriteLine("Вставка в LinkedList работает быстрее!!!");
+            }
+            else if (timeLinkedListInsert.Elapsed.TotalNanoseconds > timeListInsert.Elapsed.TotalNanoseconds)
+            {
+                Console.WriteLine("Вставка в List работает быстрее!!!");
+            }
+            else
+            {
+                Console.WriteLine("Вставка в List и в LinkedList работают одинаково...");
+            }
+            #endregion
         }
     }
 }
